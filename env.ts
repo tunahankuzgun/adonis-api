5c69ca05eb98a552eb512953bacda6845259d8c8
TK <tunahankuzgun239@gmail.com>
2022-08-09 21:36:51 +0300

    Add database model and routes for posts
    Co-authored-by: fkakkus@hotmail.com <fkakkus@hotmail.com>

--- env.ts
+++ env.ts
@@ -12,7 +12,7 @@
 |
 */
 
-import Env from '@ioc:Adonis/Core/Env'
+import Env from '@ioc:Adonis/Core/Env';
 
 export default Env.rules({
   HOST: Env.schema.string({ format: 'host' }),
@@ -21,4 +21,5 @@ export default Env.rules({
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-})
+  DB_CONNECTION: Env.schema.string(),
+});
--- start/routes.ts
+++ start/routes.ts
@@ -18,8 +18,67 @@
 |
 */
 
-import Route from '@ioc:Adonis/Core/Route'
+import Route from '@ioc:Adonis/Core/Route';
+import Post from 'App/Models/Post';
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.get('/', async () => ({ hello: 'world' }));
+
+Route.get('posts', async ({ response }) => {
+  try {
+    const posts = await Post.all();
+
+    return response.ok(posts);
+  } catch (error) {
+    return response.badRequest({ error: error.message });
+  }
+});
+
+Route.post('posts', async ({ request, response }) => {
+  try {
+    const title = request.input('title', '');
+    const description = request.input('description', '');
+    const image = request.input('image', '');
+
+    const post = new Post();
+    post.merge({ title, description, image });
+    await post.save();
+
+    return response.ok(post.toJSON());
+  } catch (error) {
+    return response.badRequest({ error: error.message });
+  }
+});
+
+Route.put('posts/:id', async ({ params, request, response }) => {
+  try {
+    const id = params.id;
+
+    const post = await Post.findOrFail(id);
+
+    const {
+      title = post.title,
+      description = post.description,
+      image = post.image,
+    } = request.only(['title', 'description', 'image']);
+
+    post.merge({ title, description, image });
+    await post.save();
+
+    return response.ok(post.toJSON());
+  } catch (error) {
+    return response.badRequest({ error: error.message });
+  }
+});
+
+Route.delete('posts/:id', async ({ params, response }) => {
+  try {
+    const id = params.id;
+
+    const post = await Post.findOrFail(id);
+    await post.delete();
+
+    return response.ok('Deleted');
+  } catch (error) {
+    return response.badRequest({ error: error.message });
+  }
+});
